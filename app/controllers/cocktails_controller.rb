5cfa85147d2f014530b24a21e3a1cd0b3f5f9050
Jo Jebsen <johannes.jebsen@icloud.com>
2017-05-25 14:26:09 +0100

    Added routes

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,46 @@
+class CocktailsController < ApplicationController
+
+before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
+
+  def index
+    def index
+      @cocktails = Cocktail.all
+    end
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    new_cocktail = Cocktail.new(cocktail_params)
+    new_cocktail.save
+    redirect_to cocktail_path(new_cocktail)
+  end
+
+  def edit
+  end
+
+  def update
+    @cocktail.update(cocktail_params)
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
+private
+
+  def set_cocktail
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :doses)
+  end
+end
